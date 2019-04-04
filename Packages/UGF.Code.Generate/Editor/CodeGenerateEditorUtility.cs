d1de21057668de1379ff9f2c5ecb20713842b825
Alexander <vorobyovvorobyov@gmail.com>
2019-04-04 21:16:14 +0300

    Change CodeGenerateEditorUtility.cs

--- Packages/UGF.Code.Generate/Editor/CodeGenerateEditorUtility.cs
+++ Packages/UGF.Code.Generate/Editor/CodeGenerateEditorUtility.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace UGF.Code.Generate.Editor
 {
     public static class CodeGenerateEditorUtility
     {
+        public static T AddGeneratedCodeLeadingTrivia<T>(T node) where T : SyntaxNode
+        {
+            return node.WithLeadingTrivia(SyntaxFactory.Comment("// THIS IS GENERATED CODE. DO NOT EDIT."),
+                SyntaxFactory.CarriageReturnLineFeed,
+                SyntaxFactory.Comment("// ReSharper disable all"),
+                SyntaxFactory.CarriageReturnLineFeed,
+                SyntaxFactory.CarriageReturnLineFeed);
+        }
+        
         public static HashSet<string> UsingDirectivesCollectUniqueNames(IEnumerable<UsingDirectiveSyntax> directives)
         {
             if (directives == null) throw new ArgumentNullException(nameof(directives));
